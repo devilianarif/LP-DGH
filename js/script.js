e61109d5603e344ebcb08b847fe6f2b4f4af681e
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-10-20 21:04:55 +0700

    auto update Mon 10/20/2025 21:04:55.72

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ window.addEventListener('resize', () => {
     height = canvas.height = window.innerHeight;
 });
 
+
 // === Character Interaction ===
 const charTrack = document.querySelector('.character-track');
 const carCard = document.querySelectorAll('.kartu-character');
