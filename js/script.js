de5068774867884a6a3345ec7c4b5640fce6a7a9
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-10-22 02:09:16 +0700

    auto update Wed 10/22/2025  2:09:16.34

--- js/script.js
+++ js/script.js
@@ -341,7 +341,7 @@ const characterDetails = [
     race: "Manusia",
     trait: "Pendiam, introspektif, dan penuh rasa bersalah.",
     background: "Arya adalah simbol perjalanan menuju pemulihan.",
-    imageDetail: "../.aset/arya3.png",
+    imageDetail: "../aset/arya3.png",
     imageBackground: "../aset/Aryak1.png",
   },
   {
