8b85af831e61b5391ab984b83403bb40dfcf8d6e
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-10-25 09:32:13 +0700

    auto update Sat 10/25/2025  9:32:13.04

--- js/script.js
+++ js/script.js
@@ -206,15 +206,6 @@ document.addEventListener("DOMContentLoaded", () => {
         "Et harum quidem rerum facilis est et expedita distinctio.",
         "Nam libero tempore, cum soluta nobis est eligendi optio."
       ]
-    },
-    {
-      img: "../images/s2.webp",
-      title: "Judul 5",
-      list: [
-        "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.",
-        "Consectetur, adipisci velit, sed quia non numquam eius modi tempora.",
-        "Labore et dolore magnam aliquam quaerat voluptatem."
-      ]
     }
   ];
 
