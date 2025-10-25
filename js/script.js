43d2d117d362c6ffd5411d0db83d0f784bb6bd13
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-10-25 09:46:58 +0700

    auto update Sat 10/25/2025  9:46:58.68

--- js/script.js
+++ js/script.js
@@ -173,7 +173,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const data = [
     {
       img: "../images/s1.webp",
-      title: "Season 1 — Di Balik Guyur Hujan",
+      title: "S 1 — Di Balik Guyur Hujan",
       list: [
         "Pertemuan Arya dengan gadis berpayung kuning di tengah hujan membuka kembali masa lalu yang terkubur. Gadis itu ternyata sudah meninggal tiga tahun lalu — dan menjadi kunci rahasia antara rasa bersalah, kehilangan, dan pengkhianatan masa kecil. Dari balik hujan, kenangan lama mulai hidup kembali."
       ]
