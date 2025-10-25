8cc81040146e062952a02fc159fd75986b9ab6a6
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-10-25 09:48:36 +0700

    auto update Sat 10/25/2025  9:48:36.72

--- js/script.js
+++ js/script.js
@@ -182,9 +182,7 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "../images/s2.webp",
       title: "Season 2 — Jalan Tanpa Ujung",
       list: [
-        ""Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
-        "Accusantium doloremque laudantium, totam rem aperiam.",
-        "Eaque ipsa quae ab illo inventore veritatis.""
+        ""
       ]
     },
     {
